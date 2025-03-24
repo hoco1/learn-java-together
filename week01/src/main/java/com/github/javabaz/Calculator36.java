21e4c878f4b0b95827d9586bc569abd34ee131d1
Norouzi <Norouzi@N-Norouzi>
2025-03-24 20:43:19 +0330

    update varargs input,add power,modulo,sqrt,and execute methods with exception handling

--- week01/src/main/java/com/github/javabaz/Calculator36.java
+++ week01/src/main/java/com/github/javabaz/Calculator36.java
@@ -1,28 +1,114 @@
 package com.github.javabaz;
 
 public class Calculator36 {
-	 public int add(int a, int b) {
-	        // TODO
-	    	return a+b;
+	private final int minArgs=2;
+	
+	public double execute(String operation,double... operands) {
+		switch(operation) {
+		case "pow":
+			if(operands.length<minArgs) {
+				throw new IllegalArgumentException("Insufficient operands. Minimum required: " + minArgs);
+			}
+			return pow(operands);
+		case "sqrt":
+			if(operands.length != 1) {
+				throw new IllegalArgumentException("Maximum number of required operands: 1");
+			}
+			return sqrt(operands[0]);
+		case "add":
+			if(operands.length<minArgs) {
+				throw new IllegalArgumentException("Insufficient operands. Minimum required: " + minArgs);
+			}
+			return add(operands);
+		case "subtract":
+			if(operands.length<minArgs) {
+				throw new IllegalArgumentException("Insufficient operands. Minimum required: " + minArgs);
+			}
+			return subtract(operands);
+		case "multiply":
+			if(operands.length<minArgs) {
+				throw new IllegalArgumentException("Insufficient operands. Minimum required: " + minArgs);
+			}
+			return multiply(operands);
+		case "divide":
+			if(operands.length<minArgs) {
+				throw new IllegalArgumentException("Insufficient operands. Minimum required: " + minArgs);
+			}
+			return divide(operands);
+		case "mod":
+			if(operands.length<minArgs) {
+				throw new IllegalArgumentException("Insufficient operands. Minimum required: " + minArgs);
+			}
+			return mod(operands);
+		default:
+			throw new UnsupportedOperationException("Operation not supported: " + operation);
+		}
+	}
+	
+	 public double add(double... operands) {
+		 double result=0;
+		 for(double operand:operands) {
+			 result+=operand;
+		 }
+	    	return result;
 	    }
 
-	    public int subtract(int a, int b) {
-	    	// TODO
-	    	return a-b;
+	 public double subtract(double... operands) {
+		 double result = operands[0];
+		 for(int i=1;i<operands.length;i++) {
+			 result-=operands[i];
+		 }
+		 return result;
 	    }
 
-	    public int multiply(int a, int b) {
-	        // TODO
-	    	return a*b;
+	 public double multiply(double... operands) {
+		 double result=1;
+		 for(double operand:operands) {
+			 result*=operand;
+		 }
+		 return result;
 	    }
 
-	    public int divide(int a, int b) {
-	        // TODO
-	    	try {
-	    		return a/b;
-	    	}catch(ArithmeticException e) {
-	    		// Exception handler
-	    		throw new ArithmeticException("Cannot divide by zero");
-	    	}
-	    }
+	 public double divide(double... operands) {
+		 double result=operands[0];
+		 for(int i=1;i<operands.length;i++) {
+			 if(operands[i]==0.0) {
+				 throw new ArithmeticException("Cannot divide by zero");
+			 }
+			 result/=operands[i];
+		 }
+		 return result;
+	 }
+	 
+	 public double mod(double... operands) {
+		 double result = operands[0];
+		 for(int i=1;i<operands.length;i++) {
+			 if(operands[i]==0.0) {
+				 throw new ArithmeticException("Cannot modulus by zero.");
+			 }
+			 result %= operands[i];
+		 }
+		 
+		 return result;
+	 }
+	 
+	 public double sqrt(double operand) {
+		 if(operand <0) {
+			 throw new ArithmeticException("Cannot calculate the square root of a negative number.");
+		 }
+		 double result = Math.sqrt(operand);
+		 return result;
+	 }
+	 
+	 public double pow(double... operands) {
+		 double result = operands[operands.length-1];
+		 for(int i=operands.length-2;i>=0;i--) {
+			 result = Math.pow(operands[i],result);
+		 }
+		 return result;
+	 }
+	 
+	 public int getMinArgs() {
+		 return this.minArgs;
+	 }
 }
